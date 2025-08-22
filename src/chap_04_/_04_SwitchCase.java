413355e880187e82eab12176da62f7cc2fb883f0
IlIllIllIlIlllII <ilillillililllii@0910@gmail.com>
2025-08-22 11:49:53 +0900

    feat: StudyJava

---
+++ src/chap_04_/_04_SwitchCase.java
@@ -0,0 +1,27 @@
+package chap_04_;
+
+public class _04_SwitchCase {
+    public static void main(String[] args) {
+        // Switch Case
+        // 1등 : 전액 장학금
+        // 2등 : 반액 장학금
+        // 3등 : 반액 장학금
+        // 그 외 : 장학금 대상 아님
+
+        // If Else 문을 이용한 방법
+        int ranking = 4; // 1등
+        if (ranking == 1) {
+            System.out.println("전액 장학금");
+        } else if (ranking == 2 || ranking == 3) {
+            System.out.println("반액 장학금");
+        } else {
+            System.out.println("장학금 대상 아님");
+        }
+        System.out.println("조회 완료 #1");
+
+        // Switch case 문을 이용
+        switch ( expression ) {
+
+        }
+    }
+}
