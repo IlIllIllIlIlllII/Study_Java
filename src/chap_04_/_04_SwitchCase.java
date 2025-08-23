007fa3d2750fa47bf5cbed56674ab809c712b677
IlIllIllIlIlllII <ilillillililllii@0910@gmail.com>
2025-08-23 09:20:58 +0900

    feat: StudyJava (for, while)

--- src/chap_04_/_04_SwitchCase.java
+++ src/chap_04_/_04_SwitchCase.java
@@ -8,7 +8,7 @@ public class _04_SwitchCase {
         // 3등 : 반액 장학금
         // 그 외 : 장학금 대상 아님
 
-        // If Else 문을 이용한 방법
+        // If Else 문을 이용 (여러 조건 또는 범위에 해당하는 조건)
         int ranking = 4; // 1등
         if (ranking == 1) {
             System.out.println("전액 장학금");
@@ -19,9 +19,65 @@ public class _04_SwitchCase {
         }
         System.out.println("조회 완료 #1");
 
-        // Switch case 문을 이용
-        switch ( expression ) {
-
+        // Switch case 문을 이용 (명확한 케이스가 있는 경우)
+        ranking = 3;
+        switch (ranking) {
+            case 1:
+                System.out.println("전액 장학금");
+                break;
+            case 2:
+                System.out.println("반액 장학금");
+                break;
+            case 3:
+                System.out.println("반액 장학금");
+                break;
+            default:
+                System.out.println("장학금 대상 아님");
         }
+        System.out.println("조회 완료 #2");switch (ranking) {
+            case 1:
+                System.out.println("전액 장학금");
+                break;
+            case 2:
+                System.out.println("반액 장학금");
+                break;
+            case 3:
+                System.out.println("반액 장학금");
+                break;
+            default:
+                System.out.println("장학금 대상 아님");
+        }
+        System.out.println("조회 완료 #2");
+
+        // case 2 와 3을 통합
+        ranking = 3;
+        switch (ranking) {
+            case 1:
+                System.out.println("전액 장학금");
+                break;
+            case 2:
+            case 3:
+                System.out.println("반액 장학금");
+                break;
+            default:
+                System.out.println("장학금 대상 아님");
+        }
+        System.out.println("조회 완료 #3");
+
+        // 중고상품의 등급에 따른 가격을 책정 (1급 : 최상, 4급 : 최하)
+        int grade = 1; // 등급
+        int price = 7000; // 기본 가격
+        switch (grade) {
+            case 1:
+                price += 1000; // price = price + 1000;
+            case 2:
+                price += 1000;
+            case 3:
+                price += 1000;
+                break;
+        }
+        System.out.println(grade + "등급 제품의 가격" + price + "원");
+        // 1등급 제품의 가격 : 10000원
+        // 2등급 제품의 가격 : 9000원
     }
 }
