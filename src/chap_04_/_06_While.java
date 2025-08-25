fb6673bfe18bbbab0607cc9f60aec753d3ebba80
IlIllIllIlIlllII <ilillillililllii0910@gmail.com>
2025-08-25 09:36:03 +0900

    feat: StudyJava

--- src/chap_04_/_06_While.java
+++ src/chap_04_/_06_While.java
@@ -11,5 +11,13 @@ public class _06_While {
             move += 3; // 3미터 이동
         }
         System.out.println("도착하였습니다.");
+
+        // 무한 루프
+        move = 0;
+        while (move < distance) { // 현재 이동 거리가 전체 거리보다 작다는건 조건이 참인 동안 반복 실행
+            System.out.println("발차기를 계속 합니다");
+            System.out.println("현재 이동 거리" + move);
+        }
+        System.out.println("도착하였습니다.");
     }
 }
