5fa296ad9d91c884120937e0d40a424871d67743
IlIllIllIlIlllII <ilillillililllii@0910@gmail.com>
2025-09-04 19:33:07 +0900

    feat: StudyJava (break)

---
+++ src/chap_04_/_10_Break.java
@@ -0,0 +1,15 @@
+package chap_04_;
+
+public class _10_Break {
+    public static void main(String[] args) {
+        // Break
+        // 치킨 집에서 매일 20마리만 판매 (1인당 1마리만 구매 가능)
+
+        // For 문
+        int max = 20;
+        for (int i = 0; i < 50; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
